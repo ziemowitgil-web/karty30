fa2ba13d5380843c6c4a187634092aa9dd9e6d69
ziemowit-gil <ziemowit.gil@gmail.com>
2025-11-04 00:27:30 +0100

    Dodanie seederów do danych testowych

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $users = [
+            [
+                'name' => 'Ewelina Testowa',
+                'email' => 'ewelina@testy.feer.org.pl',
+                'password' => Hash::make('Y)%[C!y@<6yZ7h-2cpBYVtLWq'),
+                'role' => 'user', // opcjonalnie, jeśli masz kolumnę role
+            ],
+            [
+                'name' => 'Developer Admin',
+                'email' => 'developeradmin@testy.feer.org.pl',
+                'password' => Hash::make('D3vAdm!n2025'),
+                'role' => 'admin', // opcjonalnie
+            ],
+        ];
+
+        foreach ($users as $user) {
+            User::updateOrCreate(
+                ['email' => $user['email']], // unikamy duplikatów
+                $user
+            );
+        }
+    }
+}
