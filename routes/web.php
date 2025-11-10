eba3fe3fcdb286ae19a2ebe09246d0e2df03d576
ziemowit-gil <ziemowit.gil@gmail.com>
2025-11-10 21:46:01 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -167,7 +167,7 @@ Route::middleware(['auth'])->group(function () {
         // Certyfikat
         Route::get('/certificate/json', [ConsultationController::class, 'certificateDetails'])->name('certificate.json');
         Route::get('/certificate', [ConsultationController::class, 'certificateDetailsView'])->name('certificate.view');
-        Route::post('/certificate/generate', [ConsultationController::class, 'generateCertificate'])->name('cg');
+        Route::post('/certificate/generate', [ConsultationController::class, 'generateCertificate'])->name('certificate.generate');
         Route::post('/certificate/access', [ConsultationController::class, 'generateCertificate'])->name('certificate.access');
 
 
