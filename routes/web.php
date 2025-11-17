b0c741ed4a7406b0ae1bf19f4d1366ded07f5879
ziemowit-gil <ziemowit.gil@gmail.com>
2025-11-17 01:19:36 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -148,11 +148,19 @@ Route::prefix('consultations')->name('consultations.')->middleware('auth')->grou
     Route::get('/{consultation}/pdf', [ConsultationController::class, 'downloadPdf'])->name('pdf');
     Route::get('/{consultation}/xml', [ConsultationController::class, 'xml'])->name('xml');
     Route::get('/{consultation}/details', [ConsultationController::class, 'details'])->name('details');
+
+    // Certyfikat użytkownika – wsteczna kompatybilność
+    Route::get('/certificate/json', [ConsultationController::class, 'certificateDetails'])->name('certificate.json');
+    Route::get('/certificate', [ConsultationController::class, 'certificateDetailsView'])->name('certificate.view');
+    Route::post('/certificate/generate', [ConsultationController::class, 'generateCertificate'])->name('certificate.generate');
+    Route::post('/certificate/access', [ConsultationController::class, 'generateCertificate'])->name('certificate.access');
+    Route::post('/certificate/revoke', [ConsultationController::class, 'revokeCertificate'])->name('certificate.revoke');
+    Route::get('/certificate/download', [ConsultationController::class, 'downloadCertificate'])->name('certificate.download');
 });
 
 /*
 |--------------------------------------------------------------------------
-| CERTYFIKATY X.509 UŻYTKOWNIKA
+| CERTYFIKATY X.509 – NOWY PANEL
 |--------------------------------------------------------------------------
 */
 Route::prefix('certificates')->name('certificates.')->middleware('auth')->group(function () {
